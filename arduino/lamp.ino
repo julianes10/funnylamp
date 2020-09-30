e71c29d2e68916d3d959ba113b68b3b589592e33
julianes10 <julianes10@yahoo.com>
2020-09-30 13:22:20 +0200

    more songs and singing temp blockingly

--- arduino/lamp.ino
+++ arduino/lamp.ino
@@ -34,7 +34,7 @@ const int PIN_SENSOR_HCSR04_ECHO    =2;  //INT NEEDED
 const int PIN_LED_STRIP             =4;
 const int PIN_LED_WHITE             =5; 
 const int PIN_SENSOR_TEMP           =6;
-const int PIN_BUZZER                =7;
+const int PIN_BUZZER                =8;
 
 // INTERRUPTS
 const int INT_SENSOR_HCSR04         =0;
@@ -43,7 +43,7 @@ const int INT_SENSOR_HCSR04         =0;
 
 // Sensors
 DHTsensor    GLBsensorDHT;
-HCSR04sensor GLBsensorHC(PIN_SENSOR_HCSR04_TRIGGER, PIN_SENSOR_HCSR04_ECHO, INT_SENSOR_HCSR04,MEDIAN_FILTER_SIZE_SENSOR_HCSR04);
+HCSR04sensor GLBsensorHC(PIN_SENSOR_HCSR04_TRIGGER,PIN_SENSOR_HCSR04_ECHO,INT_SENSOR_HCSR04,MEDIAN_FILTER_SIZE_SENSOR_HCSR04);
 
 // Misc 
 unsigned int aliveLoopCounter=0;
@@ -52,7 +52,7 @@ unsigned int sCounter=0;
 unsigned int latestTempReported=0;
 
 
-char    GLBauxString[100];  //for copying PROGMEM to RAM, use it in rtt and ls to save ram and avoid corruption issues.
+char    GLBauxString[100];  //for copying PROGMEM to RAM,use it in rtt and ls to save ram and avoid corruption issues.
 
 #ifdef ENABLE_SERIAL_INPUT
 char    GLBserialInputString[100]; // a string to hold incoming data
@@ -64,17 +64,17 @@ const char inputBootString[] PROGMEM ={":LP0020:LT0005:LMK:LCFF,00,00:LQ:LMk:LQ:
 const char inputTest[]  PROGMEM = {":LP0200:LT0050:LMN:LQ:LMA:LP0100:LCFF,00,00:LQ:LMA:LP0100:LC00,FF,00:LQ:LMA:LP0100:LC00,00,FF"};
 
 const char LSdistanceZero[]  PROGMEM = {":LMN:LP0000"};
-const char LSdistanceA[]  PROGMEM = {":LMA:LP0000:LCFF,00,00"};
-const char LSdistanceB[]  PROGMEM = {":LMA:LP0000:LC00,FF,00"};
-const char LSdistanceC[]  PROGMEM = {":LMA:LP0000:LC00,00,FF"};
+const char LSdistanceA[]  PROGMEM = {":LX:LMK:LP0000:LT0010:LCFF,00,00"};
+const char LSdistanceB[]  PROGMEM = {":LX:LMK:LP0000:LT0010,LC00,FF,00"};
+const char LSdistanceC[]  PROGMEM = {":LX:LMK:LP0000:LT0010,LC00,00,FF"};
 const char LSoff[]  PROGMEM = {":LX"};
 
-const char LStempA[]  PROGMEM = {":LX:LMA:LP0000:LT0000:LC33,33,FF"};
-const char LStempB[]  PROGMEM = {":LX:LMA:LP0000:LT0000:LC99,99,FF"};
-const char LStempC[]  PROGMEM = {":LX:LMA:LP0000:LT0000:LCB3,FF,B3"};
-const char LStempD[]  PROGMEM = {":LX:LMA:LP0000:LT0000:LCFF,FF,99"};
-const char LStempE[]  PROGMEM = {":LX:LMA:LP0000:LT0000:LCFF,CC,66"};
-const char LStempF[]  PROGMEM = {":LX:LMA:LP0000:LT0000:LCFF,5C,33"};
+const char LStempA[]  PROGMEM = {":LX:LMA:LP0000:LT0000:LC50,50,FF"};
+const char LStempB[]  PROGMEM = {":LX:LMA:LP0000:LT0000:LC46,82,82"};
+const char LStempC[]  PROGMEM = {":LX:LMA:LP0000:LT0000:LC73,D1,8B"};
+const char LStempD[]  PROGMEM = {":LX:LMA:LP0000:LT0000:LCEE,F7,70"};
+const char LStempE[]  PROGMEM = {":LX:LMA:LP0000:LT0000:LCF2,AD,35"};
+const char LStempF[]  PROGMEM = {":LX:LMA:LP0000:LT0000:LCFF,50,20"};
 
 
 
@@ -86,7 +86,7 @@ void(* resetFunc) (void) = 0;//declare reset function at address 0
 void GLBcallbackTimeoutLS(void);
 void GLBcallbackPauseLS(void);
 
-LSEM GLBledStrip(GLBledStripBufferMem, NUM_LEDS, GLBcallbackPauseLS, GLBcallbackTimeoutLS);
+LSEM GLBledStrip(GLBledStripBufferMem,NUM_LEDS,GLBcallbackPauseLS,GLBcallbackTimeoutLS);
 
 unsigned long time;
 SimpleTimer mainTimers;
@@ -104,12 +104,21 @@ const char  StarWars[] PROGMEM = {"StarWars:d=4,o=5,b=45:32p,32f#,32f#,32f#,8b.,
 const char  AbbaMammaMia[] PROGMEM = {"AbbaMammaMia:d=4,o=5,b=40:32f6,32d#6,32f6,32d#6,8p,32d#6,32d#6,32f6,32g6,32f6,32d#6,16p,16f6,32d#6,16p,16g#6,32g#6,32g#6,32g#6,16g6,16d#6,16p,32f6,32d#6,32f6,32d#6,8p,32d#6,32d#6,32f6,32g6,32f6,32d#6,16p,16f6,32d#6,16p,16g#6,32g#6,32g#6,32g#6,16g6,16d#6,16p,16a#.6,32a#6,32a#6,16a#6,16f6,16g6,16g#6,16p,32p,16g.6,32g6,32g6,16g6,16d6,16d#6,16f6,16p,32p,16f6,16d#6,32p,16g#6,32g#6,32g#6,32g#6,32g6,32d#6,32f6,32d#6\0"};
 
 
-
-const char warmingUp[] PROGMEM ={"marioShort:d=4,o=5,b=100:16e6,16e6,32p,8e6,16c6,8e6,8g6,8p,8g\0"};
-const char warmingUp2[] ={"marioShort:d=4,o=5,b=100:16e6,16e6,32p,8e6,16c6,8e6,8g6,8p,8g\0"};
+const char  Imperial[] PROGMEM = {"Imperial:d=4,o=5,b=100:e,e,e,8c,16p,16g,e,8c,16p,16g,e,p,b,b,b,8c6,16p,16g,d#,8c,16p,16g,e,8p\0"};
 
 
-#define MAX_AUDIOS 5
+
+const char  Supercalif[] PROGMEM = {"Supercalif:d=32,o=5,b=45:f,g#,g#,g#,a#,g#,g#,f,g#,g#,a#,g#,16g#,16f#,g#,g#,g#,g#,a#,g#,g#,d#,g#,g#,a#,g#,16g#,16f,g#,g#,g#,g#,a#,g#,g#,g#,c#6,c#6,d#6,c#6,16c#6,16a#,a#,c#6,c6,a#,c#6,g#,g#,f,g#,a,a#,c6,16c#6,16c#6\0"};
+
+
+
+const char  Twinkle[] PROGMEM = {"Twinkle:d=4,o=5,b=80:32p,8c,8c,8g,8g,8a,8a,g,8f,8f,8e,8e,8d,8d,c,8g,8g,8f,8f,8e,8e,d,8g,8g,8f,8f,8e,8e,d,8c,8c,8g,8g,8a,8a,g\0"};
+
+const char warmingUp[] PROGMEM ={"TwinkleShort:d=4,o=5,b=80:32p,8c,8c,8g,8g,8a,8a,g\0"};
+
+
+
+#define MAX_AUDIOS 8
 /* ALTERNATIVE WITHOUT TRACKING 
 const char *audiosTable[MAX_AUDIOS]= {
   mario,mi,indiana
@@ -187,7 +196,7 @@ void GLBcallbackLoggingTemp(void)
 void GLBcallbackLoggingLedWhite(void)
 {
   Serial.println(F("DEBUG: Led white..."));
-  digitalWrite(PIN_LED_WHITE, !digitalRead(PIN_LED_WHITE));
+  digitalWrite(PIN_LED_WHITE,!digitalRead(PIN_LED_WHITE));
 }
 //------------------------------------------------
 void GLBcallbackLoggingLedStrip(void)
@@ -205,6 +214,32 @@ void GLBcallbackLoggingUltrasonic(void)
     GLBsensorHC.trigger();  
   }
 }
+
+
+//------------------------------------------------
+void GLBsingTemp()
+{
+  float t=GLBsensorDHT.getTemperature();
+  float h=GLBsensorDHT.getHumidity();
+
+  int dec = (int) t/10;
+  int units= (t - dec*10);
+  int i=0;
+
+
+
+  for (i=0;i<dec;i++){
+    tone(PIN_BUZZER,1000,20);
+    delay(100);
+  }
+
+  delay(500);
+  for (i=0;i<units;i++){
+    tone(PIN_BUZZER,2000,20);
+    delay(100);
+  } 
+
+}
 //------------------------------------------------
 //------------------------------------------------
 //------------------------------------------------
@@ -212,34 +247,36 @@ void GLBcallbackLoggingUltrasonic(void)
 void setup() { 
   // Serial to debug AND comunication protocolo with PI              
   Serial.begin(9600);
-  Serial.println(F("Setup... 'came on, be my baby, came on'"));
+  Serial.println(F("Setup... 'came on,be my baby,came on'"));
 
 #ifdef ENABLE_SERIAL_INPUT
   GLBserialInputString[0]=0;
 #endif 
   GLBptrStateFunc = STATE_init;
   Serial.println(F("STATE INIT"));
-  FastLED.addLeds<WS2812B,PIN_LED_STRIP,GRB>(GLBledStripBufferMem, NUM_LEDS);
+  FastLED.addLeds<WS2812B,PIN_LED_STRIP,GRB>(GLBledStripBufferMem,NUM_LEDS);
 
   Serial.println("Setup DHT22 ");
   if (GLBsensorDHT.setup(PIN_SENSOR_TEMP)!=0) {
       Serial.print("Failing dht22 setup on pin:");
       Serial.println(PIN_SENSOR_TEMP);
   }
-  pinMode(PIN_LED_WHITE, OUTPUT);
+  pinMode(PIN_LED_WHITE,OUTPUT);
   GLBsensorHC.setup();
   GLBsensorHC.trigger();  
   /* TODO constant sensor testing 
   mainTimers.setInterval(2000,GLBcallbackLogging);
 
-  mainTimers.setInterval(7000,GLBcallbackLoggingTemp);
+
 
   mainTimers.setInterval(5000,GLBcallbackLoggingLedWhite);
 
   mainTimers.setInterval(10000,GLBcallbackLoggingLedStrip);
   mainTimers.setInterval(1000,GLBcallbackLoggingUltrasonic);
-
+  mainTimers.setInterval(5000,GLBcallbackLoggingTemp);
   */
+
+
 
   randomSeed(analogRead(0));
 
@@ -250,10 +287,15 @@ void setup() {
   audiosTracker.addItem(indiana);
   audiosTracker.addItem(StarWars);
   audiosTracker.addItem(AbbaMammaMia);
+  audiosTracker.addItem(Imperial);
+  audiosTracker.addItem(Supercalif);
+  audiosTracker.addItem(Twinkle);
 
 
 
-  //REMINDER IF ADD MORE, MAKE SURE MAX_AUDIOS is sync
+
+
+  //REMINDER IF ADD MORE,MAKE SURE MAX_AUDIOS is sync
 
 }
 
@@ -292,7 +334,7 @@ void STATE_init(void)
   #ifdef LAMP_STATES_DEBUG
   Serial.println(F("STATE INIT -> WELCOME"));
   #endif
-  tone(PIN_BUZZER, 1000, 20);
+  tone(PIN_BUZZER,1000,20);
 
   digitalWrite(PIN_LED_WHITE,HIGH);
   delay(500);
@@ -302,14 +344,16 @@ void STATE_init(void)
 
   sendFlashStringLS(inputBootString);
   GLBptrStateFunc=STATE_welcome;
-//  rtttl::beginF(PIN_BUZZER, warmingUp);
-  rtttl::begin(PIN_BUZZER, warmingUp2);
+  rtttl::beginF(PIN_BUZZER,warmingUp);
 
 }
 //-------------------------------------------------
 void STATE_welcome(void)
 {
   if (GLBledStrip.isIdle() && !rtttl::isPlaying()) {
+    //Force a pause TODO ADD A NEW STAGE AND NON-BLOCKING SING TEMP
+    delay(1000);
+    GLBsingTemp();
     GLBptrStateFunc=STATE_idle;
 #ifdef LAMP_STATES_DEBUG
     Serial.println(F("STATE WELCOME -> IDLE"));
@@ -418,10 +462,10 @@ void STATE_distanceZero(void)
 #endif
 #endif
     GLBptrStateFunc=STATE_idle; 
-    //tone(PIN_BUZZER, 2000, 50); 
+    //tone(PIN_BUZZER,2000,50); 
     return;
   } 
-  rtttl::stop(); //Just in case, silence pls
+  rtttl::stop(); //Just in case,silence pls
 }
 
 //-------------------------------------------------
@@ -443,7 +487,7 @@ void STATE_distanceA(void)
     GLBptrStateFunc=STATE_distanceZero; 
     sendFlashStringLS(LSdistanceZero);  
     rtttl::stop(); 
-    //tone(PIN_BUZZER, 2000, 50);  
+    //tone(PIN_BUZZER,2000,50);  
     return;
   } 
   if (d < DISTANCE_A) {
@@ -461,7 +505,7 @@ void STATE_distanceA(void)
 #endif
 #endif
     GLBptrStateFunc=STATE_singing;
-    //tone(PIN_BUZZER, 2000, 50); 
+    //tone(PIN_BUZZER,2000,50); 
     return;
   }
   else {
@@ -473,7 +517,7 @@ void STATE_distanceA(void)
     Serial.println(d2);
 #endif
 #endif
-    //tone(PIN_BUZZER, 2000, 50);  
+    //tone(PIN_BUZZER,2000,50);  
   }
 }
 
@@ -496,7 +540,7 @@ void STATE_singing(void)
     GLBptrStateFunc=STATE_distanceZero; 
     sendFlashStringLS(LSdistanceZero);  
     rtttl::stop(); 
-    //tone(PIN_BUZZER, 2000, 50); 
+    //tone(PIN_BUZZER,2000,50); 
     return;
   } 
   if (d < DISTANCE_A) {
@@ -510,9 +554,9 @@ void STATE_singing(void)
 #endif
     GLBptrStateFunc=STATE_distanceA;
     sendFlashStringLS(LSdistanceA);  
-    //tone(PIN_BUZZER, 2000, 20); 
+    //tone(PIN_BUZZER,2000,20); 
     //delay(20);
-    //tone(PIN_BUZZER, 2000, 20); 
+    //tone(PIN_BUZZER,2000,20); 
     //delay(20);
 //    rtttl::beginF(PIN_BUZZER,audiosTracker.getRandomItem(false,true));
     rtttl::beginF(PIN_BUZZER,audiosTracker.getOrderedItem(true));  
